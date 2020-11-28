274f2e7e2c0c073fb5897b605b141a3640c420b1
hstrychalski <hubert.strychalski@centra.com>
2020-11-28 22:40:09 +0100

    add fetch

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
 
     const TABLE_ID = 'example';
+    const DATA_SOURCE_URL = 'url-placeholder';
 
     function initDataTable()
     {
@@ -28,7 +29,7 @@ $(document).ready(function() {
             "paging": false
         });
 
-        
+
         $('#input-Score').hide();
         $('#input-Link').hide();
     }
@@ -36,74 +37,10 @@ $(document).ready(function() {
 
     function fetchJson()
     {
-        return new Promise(function(resolve) {
-            let json = [
-                {
-                    "enhanced_info": null,
-                    "jurisdiction": "CzechRepublic",
-                    "link": "https://www.cnb.cz/en/supervision-financial-market/consumer-protection-and-financial-literacy/consumer-protection/notices-about-activities/Notice-about-the-activities-of-COOPER-MARKETS-s.r.o./",
-                    "name": "COOPER MARKETS s.r.o ",
-                    "regulator": "Czech National Bank",
-                    "whois": {
-                        "creation_date": "1997-03-06 01:00:00",
-                        "domain_name": "cnb.cz",
-                        "expiration_date": "2024-10-10 00:00:00",
-                        "name_servers": [
-                            "tm0.cnb.cz (89.233.135.60, 2001:b80:a0:2e::53)",
-                            "tm1.cnb.cz (195.144.124.2, 2001:af0:f:ffdb::53)"
-                        ],
-                        "registrant_name": "SB:TM358-RIPE_XX",
-                        "registrar": "REG-CZNIC",
-                        "updated_date": [
-                            "2020-04-04 08:27:05",
-                            "2018-05-15 21:32:00",
-                            "2020-03-31 14:26:07"
-                        ]
-                    }
-                },
-                {
-                    "enhanced_info": {
-                        "address": "Dreik\u00f6nigstrasse 36, Z\u00fcrich",
-                        "website": "www.zurichmarkets.com"
-                    },
-                    "jurisdiction": "Switzerland",
-                    "link": "https://www.finma.ch/en/finma-public/warning-list/zurich-markets-limited/",
-                    "name": "Zurich Markets Limited ",
-                    "regulator": "Swiss Financial Market Supervisory Authority",
-                    "whois": {
-                        "creation_date": null,
-                        "dnssec": null,
-                        "domain_name": null,
-                        "name_servers": null,
-                        "registrant_address": null,
-                        "registrant_name": null,
-                        "registrar": null,
-                        "tech-c": null
-                    }
-                },
-                {
-                    "enhanced_info": {
-                        "address": "Route de Meyrin 267, 1217 Meyrin",
-                        "website": "www.inquot.net"
-                    },
-                    "jurisdiction": "Switzerland",
-                    "link": "https://www.finma.ch/en/finma-public/warning-list/inquot-investing-group/",
-                    "name": "Inquot Investing Group ",
-                    "regulator": "Swiss Financial Market Supervisory Authority",
-                    "whois": {
-                        "creation_date": null,
-                        "dnssec": null,
-                        "domain_name": null,
-                        "name_servers": null,
-                        "registrant_address": null,
-                        "registrant_name": null,
-                        "registrar": null,
-                        "tech-c": null
-                    }
-                }
-            ];
-            resolve(json);
-        })
+        return fetch(DATA_SOURCE_URL)
+            .then(
+                response => response.json()
+            );
     }
 
     function insertCompanyTableRow(company) {
