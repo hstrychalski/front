914d98332f610b525e00c7ffe7d6033761248285
hstrychalski <hubert.strychalski@centra.com>
2020-11-28 22:04:59 +0100

    add function for datatable

--- index.js
+++ index.js
@@ -2,6 +2,31 @@ $(document).ready(function() {
 
     const TABLE_ID = 'example';
 
+    function initDataTable()
+    {
+        $('#example tfoot th').each( function () {
+            var title = $(this).text();
+            $(this).html( '<input type="text" placeholder="Search '+ title +'" />' );
+        } );
+
+        $('#example').DataTable({
+            initComplete: function () {
+                // Apply the search
+                this.api().columns().every( function () {
+                    var that = this;
+
+                    $( 'input', this.footer() ).on( 'keyup change clear', function () {
+                        if ( that.search() !== this.value ) {
+                            that
+                                .search( this.value )
+                                .draw();
+                        }
+                    } );
+                } );
+            }
+        });
+    }
+
     function fetchJson()
     {
         return new Promise(function(resolve) {
@@ -111,37 +136,9 @@ $(document).ready(function() {
                 })
             })
             .then(function() {
-
-                $('#example tfoot th').each( function () {
-                    var title = $(this).text();
-                    $(this).html( '<input type="text" placeholder="Search '+ title +'" />' );
-                } );
-
-                var table = $('#example').DataTable({
-                    initComplete: function () {
-                        // Apply the search
-                        this.api().columns().every( function () {
-                            var that = this;
-
-                            $( 'input', this.footer() ).on( 'keyup change clear', function () {
-                                if ( that.search() !== this.value ) {
-                                    that
-                                        .search( this.value )
-                                        .draw();
-                                }
-                            } );
-                        } );
-                    }
-                });
-
+               initDataTable();
             });
     }
 
-    function initDataTable()
-    {
-
-    }
-
     loadDataToTable();
-
 } );
