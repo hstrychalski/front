444d664295944a080e54b5e38809d3026b90d377
hstrychalski <hubert.strychalski@centra.com>
2020-11-28 22:33:56 +0100

    remove unnecessary stuff

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ $(document).ready(function() {
 
     function initDataTable()
     {
+
         $('#example tfoot th').each( function () {
             var title = $(this).text();
-            $(this).html( '<input type="text" placeholder="Search '+ title +'" />' );
+            $(this).html( '<input type="text" id="input-' + title + '" ' + 'placeholder="Search '+ title +'" />' );
         } );
 
         $('#example').DataTable({
@@ -14,19 +15,24 @@ $(document).ready(function() {
                 // Apply the search
                 this.api().columns().every( function () {
                     var that = this;
-
                     $( 'input', this.footer() ).on( 'keyup change clear', function () {
                         if ( that.search() !== this.value ) {
                             that
                                 .search( this.value )
                                 .draw();
                         }
-                    } );
-                } );
+                    });
+
+                });
             },
             "paging": false
         });
+
+        
+        $('#input-Score').hide();
+        $('#input-Link').hide();
     }
+
 
     function fetchJson()
     {
